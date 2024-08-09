1d02865bbc7f50fda367d552c936abb84c75c4ba
Cláudio Vinícius <claudiodev175@gmail.com>
2024-08-09 15:41:42 -0300

    Criação da Classe model e dos atributos id,nome,descrição e preço

---
+++ src/main/java/com/produtos/api/model/produto.java
@@ -0,0 +1,13 @@
+package com.produtos.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(tb_produtos)
+public class produto {
+    
+}
