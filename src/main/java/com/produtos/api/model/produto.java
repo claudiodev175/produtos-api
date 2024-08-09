bfa84eef03b3a5d1c7c2fd70e5985b50d94d521b
Cláudio Vinícius <claudiodev175@gmail.com>
2024-08-09 15:56:55 -0300

    Criação da Classe model e 4 atributos(ID,Nome,Descrição e Preço)

--- src/main/java/com/produtos/api/model/produto.java
+++ src/main/java/com/produtos/api/model/produto.java
@@ -1,5 +1,10 @@
 package com.produtos.api.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +12,22 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(tb_produtos)
+@Entity(name = "tb_produtos")
 public class produto {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_PRODUTO")
+    private Long id;
+
+    @Column(name = "NOME_PRODUTO",nullable = false)
+    private String nome;
+
+    @Column(name = "DESCRICAO_PRODUTO",nullable = false)
+    private String descricao;
+
+    @Column(name = "PRECO_PRODUTO",nullable = false)
+    private int preco;
+
+
 }
