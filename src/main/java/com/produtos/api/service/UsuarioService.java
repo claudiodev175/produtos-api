0defe4800084f1752c50ac45016bbba7bdcff5b4
Cláudio Vinícius <claudiodev175@gmail.com>
2024-08-18 10:56:10 -0300

    Adição da classe UsuarioService.java

---
+++ src/main/java/com/produtos/api/service/UsuarioService.java
@@ -0,0 +1,52 @@
+package com.produtos.api.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.produtos.api.dto.UsuarioDTO;
+import com.produtos.api.model.Usuario;
+import com.produtos.api.repository.UsuarioRepository;
+
+public class UsuarioService {
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario salvarUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public UsuarioDTO buscarUsuarioPeloId(Long id){
+        Optional<Usuario> usuarioOpt = usuarioRepository.findById(id);
+
+        if (usuarioOpt.isPresent()) {
+            return usuarioOpt.get().toDTO();
+        }
+
+        return null;
+
+    }
+
+    public void deletarUsuario(Long id){
+        usuarioRepository.deleteById(id);
+    }
+
+    public Usuario atualizarUsuario(Long id, Usuario dadosUsuario){
+        Optional<Usuario> usuarioOpt = usuarioRepository.findById(id);
+
+        if (usuarioOpt.isPresent()) {
+            Usuario usuario = usuarioOpt.get();
+
+            usuario.setNome(dadosUsuario.getNome());
+            usuario.setDatadenascimento(dadosUsuario.getDatadenascimento());
+            usuario.setCpf(dadosUsuario.getCpf());
+            usuario.setEmail(dadosUsuario.getEmail());
+
+            return usuarioRepository.save(usuario);
+        }
+
+        return null;
+        
+    }
+}
