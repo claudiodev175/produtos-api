e97e8db18429e5cd6d131eebcd39af44989b87eb
Cláudio Vinícius <claudiodev175@gmail.com>
2024-08-20 18:53:54 -0300

    Criação do método de consulta findByRegistro

--- src/main/java/com/produtos/api/repository/UsuarioRepository.java
+++ src/main/java/com/produtos/api/repository/UsuarioRepository.java
@@ -1,11 +1,14 @@
 package com.produtos.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.produtos.api.constants.RegistroAtivo;
 import com.produtos.api.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-    
+    List<Usuario> findByRegistro(RegistroAtivo registro);
 }
